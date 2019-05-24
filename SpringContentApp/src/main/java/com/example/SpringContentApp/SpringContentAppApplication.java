0f20ee47c6d1c4d80d297dcd555d0dcc7b44b36c
ngurung <nishan.gurung2013@gmail.com>
2019-05-24 15:31:22 -0500

    second round

---
+++ SpringContentApp/src/main/java/com/example/SpringContentApp/SpringContentAppApplication.java
@@ -0,0 +1,15 @@
+package com.example.SpringContentApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+@SpringBootApplication
+public class SpringContentAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringContentAppApplication.class, args);
+	}
+
+}
